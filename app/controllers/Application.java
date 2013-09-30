5de68077a5c06578ff41a9b322606d802ee7d1d6
Cleiton Pipino Queiroz <cleiton@inovare.net>
2013-09-29 21:20:14 -0300

    tela de consulta de local

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.*;
 import play.*;
 import play.mvc.*;
 import models.*;
@@ -8,7 +9,8 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        List<Local> locais = Local.find.all();
+        return ok(index.render(locais));
     }
     
     public static Result javascriptRoutes() {
