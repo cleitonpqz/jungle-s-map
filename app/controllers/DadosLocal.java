b81c9cfb878519437cd77dc0611f64e9960a027f
Cleverson Medeiros <medeiros.cleverson@gmail.com>
2013-10-28 23:02:12 -0200

    layout cadastrar dodos local

---
+++ app/controllers/DadosLocal.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import java.util.*;
+import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
+import play.*;
+import views.html.dados.*;
+import javax.persistence.PersistenceException;
+
+import models.*;
+
+public class DadosLocal extends Controller {
+
+	public static Result GO_HOME = redirect(routes.Application.index());
+	
+	public static Result index() {
+	    return GO_HOME;
+	}
+
+	public static Result novo(long id){
+		return ok( novo.render());
+	}
+
+}
